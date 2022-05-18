d51db2c4bad57b11c10a7e84eaab224e7682f3aa
Vidar Eldøy <vidar@eldoy.com>
2022-05-18 13:26:29 +0200

    Squash dispose

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+const data = require('./lib/data.js')
 const observe = require('./lib/observe.js')
 const computed = require('./lib/computed.js')
-const dispose = require('./lib/dispose.js')
+
+
+
+function dispose(computedFunction) {
+  data.computedDependenciesTracker.delete(computedFunction)
+  return computedFunction.__disposed = true
+}
+
 
 module.exports = {
   observe,
